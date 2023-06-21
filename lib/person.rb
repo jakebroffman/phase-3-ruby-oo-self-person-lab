701c7e84ac55a94706582d7499fc97216d29fac8
jakebroffman <jake.broffman1088@gmail.com>
2023-06-21 17:18:24 -0400

    commit tree a748f412fe30191aec679622f5b8aee08179874d

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,86 @@
 # your code goes here
+require 'pry'
+
+class Person
+
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize (name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(num)
+        @happiness = num 
+       if (num > 10)
+        @happiness = 10
+       elsif (num < 0)
+        @happiness = 0
+       end
+    end
+
+    def hygiene=(hygiene)
+        @hygiene = hygiene
+        if (hygiene > 10)
+            @hygiene = 10
+        elsif (hygiene < 0)
+            @hygiene = 0
+        end
+    end
+
+
+    def get_paid (amount)
+        @bank_account += amount
+        return "all about the benjamins"
+    end
+
+    def happy?
+        if @happiness > 7
+            return true
+        else return false
+        end
+    end
+
+    def clean?
+        if hygiene > 7
+            return true
+        else return false
+        end
+    end
+
+    def take_bath
+        self.hygiene=(@hygiene += 4)
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend (friend)
+        self.happiness += 3
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation (person, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            person.happiness -= 2
+            return "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            person.happiness += 1
+            return "blah blah sun blah rain"
+        else return "blah blah blah blah blah"
+        end
+    end
+
+    def name
+        @name
+    end
+end
